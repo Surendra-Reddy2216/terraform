d79649f03fd25d2f4606038ee40aa68b44e5dbbb
Surendra-Reddy2216 <reddy110522@gmail.com>
2025-05-31 12:41:41 +0530

    changes in main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ubuntu_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  key_name      = "terraform-key"
   tags = {
     Name = "Ubuntu-EC2"
   }
